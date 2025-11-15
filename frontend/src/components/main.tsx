4c65ad6df8d902654310206f7991a8960ededdf3
Unique <75515676+0Unique@users.noreply.github.com>
2025-11-14 20:01:43 -0500

    Update main.tsx

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -46,7 +46,9 @@ export function Main() {
         var element = document.querySelector('.demo-page-container');
         panzoom(element, {
           onDoubleClick: function(e) {
-            return false; // stops double click zoom
+            // `e` - is current double click event.
+
+            return false; // tells the library to not preventDefault, and not stop propagation
           }
         });
       </script>
