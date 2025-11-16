d908f354acca896d3156a66dc5b0e8e7168a1aeb
Unique <75515676+0Unique@users.noreply.github.com>
2025-11-15 21:04:39 -0500

    Update personalization.tsx

--- frontend/src/components/settings-menus/personalization.tsx
+++ frontend/src/components/settings-menus/personalization.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'preact/hooks';
-import './personalization.css';
-import { FormEvent } from 'preact/compat';
+import { useState } from "preact/hooks";
+import "./personalization.css";
+import { FormEvent } from "preact/compat";
 
 export default function PersonalizationSettingsMenu() {
-  const [firmwareSettings, setFirmwareSettings] = useState<WebMelonFirmwareSettings>(
-    window.WebMelon.firmware.getFirmwareSettings()
-  );
+  const [firmwareSettings, setFirmwareSettings] =
+    useState<WebMelonFirmwareSettings>(
+      window.WebMelon.firmware.getFirmwareSettings(),
+    );
 
   const setLanguage = (event: FormEvent<HTMLSelectElement>) => {
     let settings = Object.assign({}, firmwareSettings);
     settings.language = parseInt(event.currentTarget.value);
     setFirmwareSettings(window.WebMelon.firmware.setFirmwareSettings(settings));
-  }
+  };
 
   const setBirthdayMonth = (event: FormEvent<HTMLSelectElement>) => {
     const birthdayMonth = parseInt(event.currentTarget.value);
@@ -19,7 +20,7 @@ export default function PersonalizationSettingsMenu() {
     settings.birthdayMonth = birthdayMonth;
     // Using 2008 as the year will let us have the leap day, which can be set as a
     // birthday on the Nintendo DS.
-    let daysInMonth = (new Date(2008, birthdayMonth, 0).getDate());
+    let daysInMonth = new Date(2008, birthdayMonth, 0).getDate();
     if (settings.birthdayDay > daysInMonth) {
       settings.birthdayDay = 1;
     }
@@ -44,10 +45,18 @@ export default function PersonalizationSettingsMenu() {
       <div className="label">
         <span class="label-text">Name</span>
       </div>
-      <input type="text" placeholder="Name" maxlength={10} value={firmwareSettings.nickname} onChange={setNickname} className="input input-bordered w-full" />
+      <input
+        type="text"
+        placeholder="Name"
+        maxlength={10}
+        value={firmwareSettings.nickname}
+        onChange={setNickname}
+        className="input input-bordered w-full"
+      />
       <div className="label">
         <span class="label-text-alt">
-          This name will be shown in games and menus inside the emulator. Max length: 10 characters.
+          This name will be shown in games and menus inside the emulator. Max
+          length: 10 characters.
         </span>
       </div>
       <div className="label">
@@ -55,44 +64,34 @@ export default function PersonalizationSettingsMenu() {
       </div>
       <div className="birthday-selection">
         <div className="birthday-selector">
-          <select className="select select-bordered w-full max-w-xs" onChange={setBirthdayMonth}>
-            {Array.from({length: 12}, (_, i) => i + 1).map((i) => (
-              <option
-                key={i}
-                selected={i === firmwareSettings.birthdayMonth}
-              >{i}</option>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={setBirthdayMonth}
+          >
+            {Array.from({ length: 12 }, (_, i) => i + 1).map((i) => (
+              <option key={i} selected={i === firmwareSettings.birthdayMonth}>
+                {i}
+              </option>
             ))}
           </select>
         </div>
         <div className="birthday-selector">
-          <select className="select select-bordered w-full max-w-xs" onChange={setBirthdayDay}>
-            {Array.from({length: (new Date(2008, firmwareSettings.birthdayMonth, 0).getDate())}, (_, i) => i + 1).map((i) => (
-              <option
-                key={i}
-                selected={i === firmwareSettings.birthdayDay}
-              >{i}</option>
-            ))}
-          </select>
-        </div>
-      </div>
-      <div className="label">
-        <span class="label-text-alt">
-          Some games may add easter eggs on this day. This doesn't have to be your real birthday.
-        </span>
-      </div>
-      <div className="label">
-        <span class="label-text">Language</span>
-      </div>
-      <div className="birthday-selection">
-        <div className="birthday-selector">
-          <select className="select select-bordered w-full max-w-xs" onChange={setLanguage}>
-            {window.WebMelon.constants.FIRMWARE_LANGUAGES.map((language) => (
-              <option
-                key={language.id}
-                selected={firmwareSettings.language === language.id}
-                value={language.id}
-              >
-                {language.name}
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={setBirthdayDay}
+          >
+            {Array.from(
+              {
+                length: new Date(
+                  2008,
+                  firmwareSettings.birthdayMonth,
+                  0,
+                ).getDate(),
+              },
+              (_, i) => i + 1,
+            ).map((i) => (
+              <option key={i} selected={i === firmwareSettings.birthdayDay}>
+                {i}
               </option>
             ))}
           </select>
@@ -100,7 +99,37 @@ export default function PersonalizationSettingsMenu() {
       </div>
       <div className="label">
         <span class="label-text-alt">
-          This language will be used as the firmware language and may be used by the games in the emulator as well.
+          Some games may add easter eggs on this day. This doesn't have to be
+          your real birthday.
+        </span>
+      </div>
+      <div className="label">
+        <span class="label-text">Language</span>
+      </div>
+      <div className="birthday-selection">
+        <div className="birthday-selector">
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={setLanguage}
+          >
+            {window.WebMelon.constants.FIRMWARE_LANGUAGES.map(
+              (language: any) => (
+                <option
+                  key={language.id}
+                  selected={firmwareSettings.language === language.id}
+                  value={language.id}
+                >
+                  {language.name}
+                </option>
+              ),
+            )}
+          </select>
+        </div>
+      </div>
+      <div className="label">
+        <span class="label-text-alt">
+          This language will be used as the firmware language and may be used by
+          the games in the emulator as well.
         </span>
       </div>
     </div>
